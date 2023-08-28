3d2c84677a8ddbefad511e1eaa95dd9dd79e3ef2
João Vitor <joaovitormrn@gmail.com>
2023-08-28 20:45:57 -0300

    Implementado metodos matematicos

--- src/test/java/br/com/erudio/controllers/StartupTests.java
+++ src/test/java/br/com/erudio/controllers/StartupTests.java
@@ -1,4 +1,4 @@
-package br.com.erudio.restwithspringbootandjavaerudio;
+package br.com.erudio.controllers;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
